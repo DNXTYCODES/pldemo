5fc5d7c4a986cf549109e0a824ce660529cb3712
MatthewCodez <mayodele113@gmail.com>
2026-04-04 14:39:36 +0100

    missing toast fixed

--- admin/src/pages/ManageImages.jsx
+++ admin/src/pages/ManageImages.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 import { backendUrl } from "../App";
 import AdminPageGuide from "../components/AdminPageGuide";
 
